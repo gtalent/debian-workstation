69fad21ca0ff9ca0bd8eaf5e9b52b340633755c5
Gary Talent <gtalent2@gmail.com>
2019-07-02 22:52:22 -0500

    Add  icecc node and scheduler, and separate cpp-dev

--- gnome.sh
+++ gnome.sh
@@ -1,5 +1,9 @@
 gsettings set org.gnome.desktop.input-sources xkb-options "['caps:swapescape']"
-gsettings set org.gnome.shell enabled-extensions "['alternate-tab@gnome-shell-extensions.gcampax.github.com', 'user-theme@gnome-shell-extensions.gcampax.github.com']"
+gsettings set org.gnome.shell enabled-extensions "['alternate-tab@gnome-shell-extensions.gcampax.github.com', 'user-theme@gnome-shell-extensions.gcampax.github.com', 'dash-to-dock@micxgx.gmail.com']"
 gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.interface gtk-theme "Arc-Dark"
 gsettings set org.gnome.shell.extensions.user-theme name "Arc-Dark"
+
+# flatpaks
+sudo flatpak install flathub org.gtk.Gtk3theme.Arc-Dark
+sudo flatpak install flathub org.gnome.Builder
