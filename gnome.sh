f7eab9faa0c654e5d3ca998a09df214dae842522
Gary Talent <gtalent2@gmail.com>
2019-07-05 13:22:28 -0500

    Address Ansible deprecations

--- gnome.sh
+++ gnome.sh
@@ -4,6 +4,13 @@ gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.interface gtk-theme "Arc-Dark"
 gsettings set org.gnome.shell.extensions.user-theme name "Arc-Dark"
 
+# setup flathub
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
 # flatpaks
 sudo flatpak install flathub org.gtk.Gtk3theme.Arc-Dark
 sudo flatpak install flathub org.gnome.Builder
+sudo flatpak install flathub com.slack.Slack
+sudo flatpak install flathub io.mgba.mGBA
+sudo flatpak install flathub org.libreoffice.LibreOffice
+sudo flatpak install flathub org.gimp.GIMP
